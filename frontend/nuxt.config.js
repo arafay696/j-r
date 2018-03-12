3a15413977b34f0feb94cc3c655b9258f9110edc
Abdul Rafay <arafay696@gmail.com>
2018-03-12 23:22:53 +0500

    fixesin

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'joinnride',
+    title: 'Ride & Share',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,6 +13,18 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    '~/assets/stylesheets/Theme36/home/home.css',
+    '~/assets/stylesheets/Theme36/home/homemenu.css',
+    '~/assets/stylesheets/Theme36/home/homemedia.css',
+    '~/assets/stylesheets/Theme36/EN/lang.css',
+    '~/assets/stylesheets/Theme36/home/application.css',
+    '~/assets/stylesheets/Theme36/home/slimmenu.css',
+    '~/assets/stylesheets/Theme36/home/application.css'
+  ],
+  js: [
+
+  ],
   /*
   ** Customize the progress bar color
   */
